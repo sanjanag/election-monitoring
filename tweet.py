52abbe83d860d103fb68f62e81e66a067032f529
Daniel Nkemelu <daniel.nkemelu@gmail.com>
2019-11-17 18:55:48 -0500

    Removed trailing one-liner

--- tweet.py
+++ tweet.py
@@ -43,7 +43,6 @@ class Tweet:
             return []
 
         window_size = 1
-        summ = 0
         for windowed_subtext in Util.get_windows(tokens, window_size):
             edges = list(combinations(windowed_subtext, 2))
             self.edges = edges
